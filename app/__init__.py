611783e9e1d3f2fd71a8828c6aeffad2ee7a08b8
homekeeper89 <homekeeper89@gmail.com>
2021-10-07 22:18:21 +0900

    [add] swagger 환경 및 설정 추가

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,16 @@
 import os
 from flask import Flask
 from .config import config
+from flasgger import Swagger
+from .utils import get_swagger_config
 
 
 def create_app(env: str = "dev") -> Flask:
     app = Flask(__name__)
     app.config.from_object(config[env])
+
+    with app.app_context():
+        Swagger(app, template=get_swagger_config())
 
     @app.route("/ping")
     def temp_ping():
---
+++ app/utils/swagger.py
@@ -0,0 +1,22 @@
+TITLE = "RUN WITH ME"
+
+
+def get_swagger_config() -> dict:
+    return {
+        "openapi": "3.0",
+        "info": {
+            "title": TITLE,
+            "description": f"{TITLE} APIDOCS",
+            "contact": {
+                "responsibleOrganization": "Me",
+                "responsibleDeveloper": "Me",
+                "email": "homekeeper89@gmail.com",
+            },
+            "termsOfService": "https://github.com/DDD-6/Reversed-666-server",
+            "version": "0.0.1",
+        },
+        "host": "TBD",  # overrides localhost:500
+        "basePath": "/api",  # base bash for blueprint registration
+        "schemes": ["http", "https"],
+        "operationId": "getmyData",
+    }
