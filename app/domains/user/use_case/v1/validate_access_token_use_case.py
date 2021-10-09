e5900f855a199eb81bbc1416799f3c4ba6b85cd5
homekeeper89 <homekeeper89@gmail.com>
2021-10-09 16:06:32 +0900

    [modify] social helper use_case 에 적용

--- app/domains/user/use_case/v1/validate_access_token_use_case.py
+++ app/domains/user/use_case/v1/validate_access_token_use_case.py
@@ -1,6 +1,22 @@
+from app.utils.social_login import BaseSocialLoginHelper
+
+
 class ValidateAccessTokenUseCase:
     def __init__(self):
         pass
 
-    def execute(self, cagegory: str, token: str):
+    def execute(self, category: str, token: str):
+
+        social_helper = self.__get_helper(category, token)
+        valid_token = social_helper.validate_token()
+
         return "SUCCESS"
+
+    def __get_helper(self, category: str, token: str):
+        for helper_cls in BaseSocialLoginHelper.__subclasses__():
+            try:
+                if helper_cls.is_matched_category(category):
+                    return helper_cls(category, token)
+            except KeyError:
+                print(f"{category} has_no_helper")
+                return ""
--- tests/domains/user/use_case/test_validate_token_use_case.py
+++ tests/domains/user/use_case/test_validate_token_use_case.py
@@ -1,4 +1,14 @@
+import pytest
 from app.domains.user.use_case.v1.validate_access_token_use_case import ValidateAccessTokenUseCase
+from app.utils.social_login import KakaoLoginHelper
+
+
+@pytest.mark.parametrize("category", [("kakao")])
+def test_get_social_helpert_should_return_expected(category):
+    uc = ValidateAccessTokenUseCase()
+    helper = uc._ValidateAccessTokenUseCase__get_helper(category, "kk")
+
+    assert isinstance(helper, KakaoLoginHelper)
 
 
 def test_use_case_should_return_data(app, get_token_data):
