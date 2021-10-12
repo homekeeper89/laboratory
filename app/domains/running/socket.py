b85adf8dc93e8d198d2d6db18b56376cd29e2daa
homekeeper89 <homekeeper89@gmail.com>
2021-10-12 22:21:35 +0900

    [modify] 불필요 테스트 & socket 삭제

--- app/domains/running/socket.py
+++ app/domains/running/socket.py
@@ -5,6 +5,9 @@ from flask_socketio import emit, join_room, leave_room, send
 from flask import Flask, session, request, json as flask_json
 import json
 
+@socketio.on('disconnect', namespace="/running")
+def on_disconnect():
+    print("disconnected")
 
 @socketio.on("send_gps_data", namespace="/running")
 def on_room_event(data):
@@ -14,6 +17,11 @@ def on_room_event(data):
     data["lot"]="bbc"
     emit("calc_data", data, room=room_id)
 
+@socketio.on("leave_room", namespace="/running")
+def on_join_room_namespace(data):
+    room_id = data["room_id"]
+    leave_room(data["room_id"])
+    emit("leave_room_success", data, room=room_id)
 
 @socketio.on("join_room", namespace="/running")
 def on_join_room_namespace(data):
@@ -36,106 +44,3 @@ def connect():
             }
         )
     )
-
-
-@socketio.event(namespace="/test")
-def connect():
-    send("connected-test")
-    send(json.dumps(request.args.to_dict(flat=False)))
-    send(
-        json.dumps(
-            {
-                h: request.headers[h]
-                for h in request.headers.keys()
-                if h not in ["Host", "Content-Type", "Content-Length"]
-            }
-        )
-    )
-
-
-@socketio.on("join room")
-def on_join_room(data):
-    room = data["room"]
-    print(f"room {room}", request.sid)
-    join_room(data["room"])
-
-
-@socketio.on("leave room")
-def on_leave_room(data):
-    leave_room(data["room"])
-
-
-@socketio.on("join room", namespace="/test")
-def on_join_room_namespace(data):
-    join_room(data["room"])
-
-
-@socketio.on("leave room", namespace="/test")
-def on_leave_room_namespace(data):
-    leave_room(data["room"])
-
-
-@socketio.on("my room event")
-def on_room_event(data):
-    room = data.pop("room")
-    print(f"custom_room {room}", request.sid)
-    # name, args:List 형태
-    emit("my room response", data, room=room)
-
-
-@socketio.on("my room namespace event", namespace="/test")
-def on_room_namespace_event(data):
-    room = data.pop("room")
-    send("room message", room=room)
-
-
-@socketio.on("connect")
-def on_connect(auth):
-    if auth != {"foo": "bar"}:  # pragma: no cover
-        return False
-    if request.args.get("fail"):
-        return False
-    send("connected")
-    send(json.dumps(request.args.to_dict(flat=False)))
-    send(
-        json.dumps(
-            {
-                h: request.headers[h]
-                for h in request.headers.keys()
-                if h not in ["Host", "Content-Type", "Content-Length"]
-            }
-        )
-    )
-
-
-@socketio.on("message")
-def handle_message(data):
-    print("received message: " + data)
-
-
-@socketio.on("message")
-def handle_message(message):
-    send(message)
-    print(">>>", message)
-
-
-@socketio.on("json")
-def handle_json(json):
-    send(json, json=True, broadcast=True)
-    print("json", json)
-
-
-@socketio.on("my custom event")
-def handle_my_custom_event(json):
-    emit("my custom response", json)
-    print("emit", json)
-
-
-@socketio.on("message", namespace="/test")
-def on_message_test(message):
-    send(message)
-
-
-@socketio.on("my custom broadcast namespace event", namespace="/test")
-def on_custom_event_broadcast_test(data):
-    emit("my custom namespace response", data, namespace="/test", broadcast=True)
--- tests/test_socket.py
+++ tests/test_socket.py
@@ -1,25 +1,43 @@
 # NOTE https://github.com/miguelgrinberg/Flask-socket_app/blob/main/test_socket_app.py
+# NOTE https://blog.naver.com/PostView.nhn?isHttpsRedirect=true&blogId=shino1025&logNo=222179697262&parentCategoryNo=&categoryNo=33&viewDate=&isShowPopularPosts=true&from=search
 
-def test_each_user_should_share_data(app, socket_app):
-    running_namespace = "/running"    
+
+def test_leaving_room_should_share_message(app, socket_app):
+    running_namespace = "/running"
     first_user = socket_app.test_client(app, namespace=running_namespace)
     second_user = socket_app.test_client(app, namespace=running_namespace)
 
     first_user.get_received(running_namespace)
     second_user.get_received(running_namespace)
 
-    join_room ="join_room"
+    join_room = "join_room"
     unique_room_id = "some_unique_id"
     first_user.emit(join_room, {"room_id": unique_room_id}, namespace=running_namespace)
     second_user.emit(join_room, {"room_id": unique_room_id}, namespace=running_namespace)
     first_user.get_received(running_namespace)
     second_user.get_received(running_namespace)
 
-    data = {
-        "room_id":unique_room_id,
-        "lat":12345,
-        "lot":123
-    }
+    data = {"room_id": unique_room_id, "lat": 12345, "lot": 123}
+    first_user.emit("leave_room", data, namespace=running_namespace)
+    res = second_user.get_received(running_namespace)
+    assert res
+
+def test_each_user_should_share_data(app, socket_app):
+    running_namespace = "/running"
+    first_user = socket_app.test_client(app, namespace=running_namespace)
+    second_user = socket_app.test_client(app, namespace=running_namespace)
+
+    first_user.get_received(running_namespace)
+    second_user.get_received(running_namespace)
+
+    join_room = "join_room"
+    unique_room_id = "some_unique_id"
+    first_user.emit(join_room, {"room_id": unique_room_id}, namespace=running_namespace)
+    second_user.emit(join_room, {"room_id": unique_room_id}, namespace=running_namespace)
+    first_user.get_received(running_namespace)
+    second_user.get_received(running_namespace)
+
+    data = {"room_id": unique_room_id, "lat": 12345, "lot": 123}
     first_user.emit("send_gps_data", data, namespace=running_namespace)
     data = second_user.get_received(running_namespace)
     assert data[0]["name"] == "calc_data"
@@ -29,14 +47,14 @@ def test_each_user_should_share_data(app, socket_app):
 
 
 def test_each_user_should_join_room(app, socket_app):
-    running_namespace = "/running"    
+    running_namespace = "/running"
     first_user = socket_app.test_client(app, namespace=running_namespace)
     second_user = socket_app.test_client(app, namespace=running_namespace)
 
     first_user.get_received(running_namespace)
     second_user.get_received(running_namespace)
 
-    join_room ="join_room"
+    join_room = "join_room"
     unique_room_id = "some_unique_id"
     first_user.emit(join_room, {"room_id": unique_room_id}, namespace=running_namespace)
     second_user.emit(join_room, {"room_id": unique_room_id}, namespace=running_namespace)
@@ -44,8 +62,9 @@ def test_each_user_should_join_room(app, socket_app):
     received = first_user.get_received(running_namespace)
     assert received[0]["name"] == "joinning_room_success"
 
+
 def test_each_user_should_connected_running_rooms(app, socket_app):
-    running_namespace = "/running"    
+    running_namespace = "/running"
     first_user = socket_app.test_client(app, namespace=running_namespace)
     second_user = socket_app.test_client(app, namespace=running_namespace)
 
@@ -53,128 +72,3 @@ def test_each_user_should_connected_running_rooms(app, socket_app):
     assert data
     data = second_user.get_received(running_namespace)
     assert data
-
-
-def test_room_should_workd(app, socket_app):
-    client1 = socket_app.test_client(app, auth={"foo": "bar"})
-    client2 = socket_app.test_client(app, auth={"foo": "bar"})
-    client3 = socket_app.test_client(app, namespace="/test")
-    client1.get_received()
-    client2.get_received()
-    res = client3.get_received("/test")
-
-    client1.emit("join room", {"room": "one"})
-    client2.emit("join room", {"room": "one"})
-    client3.emit("join room", {"room": "one"}, namespace="/test")
-    client1.emit("my room event", {"a": "b", "room": "one"})
-    received = client1.get_received()
-    assert len(received) == 1
-    assert len(received[0]["args"]) == 1
-    assert received[0]["name"] == "my room response"
-    assert received[0]["args"][0]["a"] == "b"
-    assert received == client2.get_received()
-    print(received)
-
-    received = client3.get_received("/test")
-    assert len(received) == 0
-
-    client1.emit("leave room", {"room": "one"})
-    client1.emit("my room event", {"a": "b", "room": "one"})
-    received = client1.get_received()
-    assert len(received) == 0
-    received = client2.get_received()
-    assert len(received) == 1
-    assert len(received[0]["args"]) == 1
-    assert received[0]["name"] == "my room response"
-    assert received[0]["args"][0]["a"] == "b"
-    print("client2", received)
-    received = client2.get_received()
-    print("client2", received)
-    client2.disconnect()
-
-    socket_app.emit("my room event", {"a": "b"}, room="one")
-    received = client1.get_received()
-    assert len(received) == 0
-    received = client3.get_received("/test")
-    assert len(received) == 0
-
-    client3.emit("my room namespace event", {"room": "one"}, namespace="/test")
-    received = client3.get_received("/test")
-    assert len(received) == 1
-    assert received[0]["name"] == "message"
-    assert received[0]["args"] == "room message"
-    socket_app.close_room("one", namespace="/test")
-    client3.emit("my room namespace event", {"room": "one"}, namespace="/test")
-    received = client3.get_received("/test")
-    assert len(received) == 0
-
-
-def test_socket_should_conneted(app, socket_app):
-    client = socket_app.test_client(app, auth={"foo": "bar"})
-    client2 = socket_app.test_client(app, auth={"foo": "bar"})
-    assert client.is_connected()
-    assert client2.is_connected()
-    assert client.eio_sid != client2.eio_sid
-
-    received = client.get_received()
-    assert len(received) == 3
-    assert received[0]["args"] == "connected"
-
-    client.disconnect()
-    assert not client.is_connected()
-    assert client2.is_connected()
-    client2.disconnect()
-    assert not client2.is_connected()
-
-
-def test_send_message_should_received(app, socket_app):
-    client = socket_app.test_client(app, auth={"foo": "bar"})
-    client.get_received()
-    client.send("echo this message back")
-    received = client.get_received()
-    assert len(received) == 1
-    assert received[0]["args"] == "echo this message back"
-
-
-def test_send_json_should_share_data(app, socket_app):
-    client1 = socket_app.test_client(app, auth={"foo": "bar"})
-    client2 = socket_app.test_client(app, auth={"foo": "bar"})
-    client1.get_received()
-    client2.get_received()
-    client1.send({"a": "b"}, json=True)
-    received = client1.get_received()
-
-    assert len(received) == 1
-    assert received[0]["args"]["a"] == "b"
-    received = client2.get_received()
-    assert len(received) == 1
-    assert received[0]["args"]["a"] == "b"
-
-
-def test_custom_event_should_work(app, socket_app):
-    client = socket_app.test_client(app, auth={"foo": "bar"})
-    client.get_received()
-    client.emit("my custom event", {"a": "b"})
-    received = client.get_received()
-    assert len(received) == 1
-    assert len(received[0]["args"]) == 1
-    assert received[0]["name"] == "my custom response"
-    assert received[0]["args"][0]["a"] == "b"
-
-
-def test_broadcast_namespace(app, socket_app):
-    client1 = socket_app.test_client(app, namespace="/test")
-    client2 = socket_app.test_client(app, namespace="/test")
-    client3 = socket_app.test_client(app, auth={"foo": "bar"})
-    received = client2.get_received("/test")
-    assert len(received) == 3
-
-    client3.get_received()
-    client1.emit("my custom broadcast namespace event", {"a": "b"}, namespace="/test")
-    received = client2.get_received("/test")
-
-    assert len(received) == 1
-    assert len(received[0]["args"]) == 1
-    assert received[0]["name"] == "my custom namespace response"
-    assert received[0]["args"][0]["a"] == "b"
-    assert len(client3.get_received()) == 0
