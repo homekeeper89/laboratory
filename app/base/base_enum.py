c879647ac5603a3521d0200ee21d0db0e8d2ed52
homekeeper89 <homekeeper89@gmail.com>
2021-10-23 17:56:58 +0900

    [modify] enum str 형태 믹스인으로 변경

--- app/base/base_enum.py
+++ app/base/base_enum.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 
-class BaseEnum(Enum):
+class BaseEnum(str, Enum):
     def _generate_next_value_(name, start, count, last_values):
         return name
 
--- tests/domains/running/view/test_room_view.py
+++ tests/domains/running/view/test_room_view.py
@@ -11,6 +11,7 @@ def test_create_room_should_return_room_id(test_client, get_json_headers):
     }
 
     res = test_client.post(endpoint, data=json.dumps(data), headers=get_json_headers)
+
     assert res.status_code == 200
     data = res.json["data"]
     assert data
