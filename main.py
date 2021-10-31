f6d6ee29623de07eaec4285be64f9d6acddea00b
homekeeper89 <homekeeper89@gmail.com>
2021-10-31 20:16:47 +0900

    [modify] env 환경 삭제된것추가

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import os
 from dotenv import load_dotenv
 
-# env = os.getenv("FLASK_ENV", "dev")
-# load_dotenv(f".env.{env}")
+env = os.getenv("FLASK_ENV", "dev")
+load_dotenv(f".env.{env}")
 
 from app import create_app, socketio
 
 if __name__ == "__main__":
-    app = create_app("dev")
+    app = create_app(env)
     socketio.run(app, port=5000)
