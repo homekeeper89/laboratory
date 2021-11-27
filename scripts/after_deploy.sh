bf619489d420c796e46acb9173e94ce732ec6462
homekeeper89 <homekeeper89@gmail.com>
2021-11-27 16:17:49 +0900

    [modify] 경로 찍게끔 수정

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-
 echo "Start Build"
 echo "Stop image"
 docker rm $(docker stop $(docker ps -a -q --filter ancestor=run_with_me --format="{{.ID}}"))
 
 echo "Delete image"
 docker rmi $(docker images | grep 'run_with_me')
+
+echo "print dir"
+pwd
 
 echo "Build image dev"
 docker-compose --env-file ../.env.dev up -d
