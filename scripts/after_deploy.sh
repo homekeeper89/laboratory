e16cd3ed438aa8e89b4d76fb21bc9077fbdc3358
homekeeper89 <homekeeper89@gmail.com>
2021-11-27 15:51:28 +0900

    [modify] image 생성 직접 명시

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -7,7 +7,7 @@ docker rm $(docker stop $(docker ps -a -q --filter ancestor=run_with_me --format
 echo "Delete image"
 docker rmi $(docker images | grep 'run_with_me')
 
-echo "Build image"
-make build_compose
+echo "Build image dev"
+docker-compose --env-file .env.dev up -d
 
 echo "Done"
