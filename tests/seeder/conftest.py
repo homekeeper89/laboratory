cffb0a647ef0ee1afa68f2115f7f3e12bf460ad2
homekeeper89 <homekeeper89@gmail.com>
2021-11-28 19:22:31 +0900

    [add] config 데이터 넣기 위한 데이터 팩토리 기본 형태 추가

--- tests/seeder/conftest.py
+++ tests/seeder/conftest.py
@@ -1,14 +1,25 @@
 import pytest
 
-from app.domains.running.enum import RunningStatusEnum
-from . import RunningFactory, RunningParticipantFactory, UserFactory
+from app.domains.running.enum import RunningModeEnum, RunningStatusEnum, RunningConfigCategoryEnum
+from . import RunningFactory, RunningParticipantFactory, UserFactory, RunningConfigFactory
+
+
+def make_models_by_category(mode: RunningModeEnum):
+    if mode == RunningModeEnum.COMPETITION:
+        return [RunningConfigFactory.build(category=RunningConfigCategoryEnum.DISTANCE)]
+    else:
+        return [
+            RunningConfigFactory.build(category=RunningConfigCategoryEnum.LIMIT_MINUTES),
+            [RunningConfigFactory.build(category=RunningConfigCategoryEnum.LIMIT_USER_COUNTS)],
+        ]
 
 
 @pytest.fixture(scope="function")
 def running_domain_factory(session):
     def _running_domain_factory(user_counts: int = 4, **kwargs):
         nonlocal session
-        res = RunningFactory.build(status=RunningStatusEnum.ATTENDING.name)
+        mode = kwargs.get("running_mode", RunningModeEnum.COMPETITION)
+        res = RunningFactory.build(status=RunningStatusEnum.ATTENDING.name, mode=mode)
         session.add(res)
         session.flush()
 
@@ -19,7 +30,9 @@ def running_domain_factory(session):
         except Exception:
             models = [model]
 
+        config_models = make_models_by_category(mode)
         session.add_all(models)
+        session.add_all(config_models)
         session.flush()
 
         for model in models:
